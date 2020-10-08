b82f8ca751cd3a8b4ef8b5146a1434b641e5bf9c
shepy02 <judani9412@gmail.com>
2020-10-08 23:47:04 +0200

    Implement CreateTable

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -24,6 +24,7 @@ namespace ExcelGeneration
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
@@ -41,7 +42,7 @@ namespace ExcelGeneration
 
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable(xlSheet);
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -56,6 +57,61 @@ namespace ExcelGeneration
                 xlWB = null;
                 xlApp = null;
             }
+        }
+
+        private void CreateTable(Excel.Worksheet worksheet)
+        {
+            string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0, j = 1; i < headers.Length; i++, j++)
+            {
+                worksheet.Cells[1, j] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int k = 0;
+            foreach (Flat flat in Flats)
+            {
+                values[k, 0] = flat.Code;
+                values[k, 1] = flat.Vendor;
+                values[k, 2] = flat.Side;
+                values[k, 3] = flat.District;
+                values[k, 4] = flat.Elevator ? "Van" : "Nincs";
+                values[k, 5] = flat.NumberOfRooms;
+                values[k, 6] = flat.FloorArea;
+                values[k, 7] = flat.Price;
+                values[k, 8] = "=" + GetCell(2+k, 8) + "*1000000/" + GetCell(2+k, 7);
+                k++;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
