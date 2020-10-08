8c4d66e873cbe235e042dc460e55c728b34207fa
shepy02 <judani9412@gmail.com>
2020-10-08 23:04:06 +0200

    Cache Flats table on form load

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -12,9 +12,18 @@ namespace ExcelGeneration
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
