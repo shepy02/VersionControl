6b24312b9db6f005ff033fb5464ecd75143ce2cb
shepy02 <judani9412@gmail.com>
2020-10-08 23:14:53 +0200

    Create instance of Excel

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -16,6 +16,9 @@ namespace ExcelGeneration
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
@@ -26,6 +29,33 @@ namespace ExcelGeneration
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel() 
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
